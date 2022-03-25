30be121bfd51881b8c6a301b1031f4872b1dbafd
Matthias Zepper <MatthiasZepper@users.noreply.github.com>
2022-03-25 18:47:54 +0100

    Fix path in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 version = {}
 package_root = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(package_root, "dds_cli/version.py")) as fp:
+with open(os.path.join(package_root, "version.py")) as fp:
     exec(fp.read(), version)
 VERSION = version["__version__"]
 
